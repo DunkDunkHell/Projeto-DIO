8f15dfb6fe38563b95dbed168ec5814b0795a7b4
DunkDunkHell <gabriellyrodriguesmartins44@gmail.com>
2026-01-22 20:10:35 -0300

    Primeiro projeto DIO

--- Projeto.js
+++ Projeto.js
@@ -1,1 +1,24 @@
-console.log("aaaaa")
+// variáveis
+let nomeHeroi = "Leon S. Kennedy"
+let quantidadeXp = 2005
+let nivel = ""
+
+// 2. Estrutura de decisão
+if (quantidadeXp < 1000) {
+    nivel = "Ferro"
+} else if (quantidadeXp >= 1001 && quantidadeXp <= 2000) {
+    nivel = "Bronze"
+} else if (quantidadeXp >= 2001 && quantidadeXp <= 5000) {
+    nivel = "Prata"
+} else if (quantidadeXp >= 5001 && quantidadeXp <= 7000) {
+    nivel = "Ouro"
+} else if (quantidadeXp >= 7001 && quantidadeXp <= 8000) {
+    nivel = "Platina"
+} else if (quantidadeXp >= 8001 && quantidadeXp <= 9000) {
+    nivel = "Ascendente"
+} else if (quantidadeXp >= 9001 && quantidadeXp <= 10000) {
+    nivel = "Imortal"
+} else 
+    nivel = "Radiante"
+
+    console.log(("O Herói de nome " + nomeHeroi) + (" está no nível " + nivel))
